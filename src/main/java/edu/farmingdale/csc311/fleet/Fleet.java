3b8f3f3f825910e06aa81911c4bc9d300d2fde16
Michael Hickey <hickmj1@farmingdale.edu>
2026-09-04 17:12:19 -0400

    TODO-09: implement Fleet reports

--- src/main/java/edu/farmingdale/csc311/fleet/Fleet.java
+++ src/main/java/edu/farmingdale/csc311/fleet/Fleet.java
@@ -114,18 +114,71 @@ public class Fleet {
      * ------------------------------------------------------------------ */
 
     public Vehicle[] sortedByYear() {
-        throw new UnsupportedOperationException("TODO-09");
+        Vehicle[] copy = toArray();
+
+        // Insertion sort ordered by year (oldest first), then make A to Z ignoring case
+        for (int i = 1; i < copy.length; i++) {
+            Vehicle key = copy[i];
+            int j = i - 1;
+            while (j >= 0) {
+                if (copy[j].getYear() > key.getYear()) {
+                    copy[j + 1] = copy[j];
+                    j--;
+                } else if (copy[j].getYear() == key.getYear()) {
+                    if (copy[j].getMake().compareToIgnoreCase(key.getMake()) > 0) {
+                        copy[j + 1] = copy[j];
+                        j--;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            copy[j + 1] = key;
+        }
+        return copy;
     }
 
     public int countWithFuelType(FuelType fuel) {
-        throw new UnsupportedOperationException("TODO-09");
+        int matchCount = 0;
+        for (int i = 0; i < count; i++) {
+            if (vehicles[i].getFuelType() == fuel) {
+                matchCount++;
+            }
+        }
+        return matchCount;
     }
 
     public double averageEngineSize() {
-        throw new UnsupportedOperationException("TODO-09");
+        double totalEngineSize = 0.0;
+        int engineCount = 0;
+        for (int i = 0; i < count; i++) {
+            if (vehicles[i].getFuelType().hasEngine()) {
+                totalEngineSize += vehicles[i].getEngineSize();
+                engineCount++;
+            }
+        }
+        if (engineCount == 0) {
+            return 0.0;
+        }
+        return totalEngineSize / engineCount;
     }
 
     public Vehicle longestRange() {
-        throw new UnsupportedOperationException("TODO-09");
+        if (count == 0) {
+            return null;
+        }
+        Vehicle longest = vehicles[0];
+        double maxRange = longest.rangeInMiles();
+
+        for (int i = 1; i < count; i++) {
+            double currentRange = vehicles[i].rangeInMiles();
+            if (currentRange > maxRange) {
+                maxRange = currentRange;
+                longest = vehicles[i];
+            }
+        }
+        return longest;
     }
 }
